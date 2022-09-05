5ee8b5a01d4c53419a4a240cc9d0b8c92425b3cd
Satria Rahmat <satria@skinmystery.com>
2022-09-05 12:08:34 +0700

    get tags

--- main.py
+++ main.py
@@ -2,46 +2,55 @@ import requests, json
 from bs4 import BeautifulSoup
 
 
+def get_tags(url):
+	tags = BeautifulSoup(requests.get(url).text, 'html.parser').find_all(class_="detail__body-tag")
+	if len(tags) < 1: return []
+	return [i.text for i in BeautifulSoup(str(tags[0]), 'html.parser').find_all("a")]
 
-def scrap(query, pagination=1):
-	url = "https://www.detik.com/search/searchall?query="+query
-	if pagination != 1 or pagination != 0:
-		url+="&sortby=time&page="+str(pagination)
-	all_news = requests.get(url).text
-	all_news = BeautifulSoup(all_news, 'html.parser')
-	all_news = all_news.find_all("article")
+
+def list_news(query, saveTo):
+	pagination = 1
 	result = []
-	for news in all_news:
-		article = BeautifulSoup(str(news.extract()), 'html.parser')
-		# Get url
-		url = article.find_all("a", href=True)
-		if len(url) > 0:
-			url = url[0]["href"]
-		else:
-			url = ""
-		# get title
-		title = article.find_all(class_="title")
-		if len(title) > 0:
-			title = title[0].text
-		else:
-			title = ""
-		# get date
-		date = article.find_all(class_="date")
-		if len(date) > 0:
-			date = date[0].text.replace("detikNews", "")
-		else:
-			date = ""
-		result.append({"title": title, "url": url, "date": date})
+	while True:
+		url = "https://www.detik.com/search/searchall?query="+query
+		if pagination != 1 or pagination != 0: url+="&sortby=time&page="+str(pagination)
+		all_news = BeautifulSoup(requests.get(url).text, 'html.parser').find_all("article")
+		if len(all_news) < 1: break
+		for news in all_news:
+			article = BeautifulSoup(str(news.extract()), 'html.parser')
+			# Get url
+			url = article.find_all("a", href=True)
+			if len(url) > 0:
+				url = url[0]["href"]
+			else:
+				url = ""
+			# get title
+			title = article.find_all(class_="title")
+			if len(title) > 0:
+				title = title[0].text
+			else:
+				title = ""
+			# get date
+			date = article.find_all(class_="date")
+			if len(date) > 0:
+				date = date[0].text.replace("detikNews", "")
+			else:
+				date = ""
+			# get tags
+			tags = get_tags(url) if url != "" else []
+
+			result.append({"title": title, "url": url, "date": date, "tags": tags})
+			json.dump(result, open(saveTo, 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
+			print(f" [{pagination}] {url}")
+		pagination+=1
 	return result
 
+
 def main():
-	saveTo = str(input(" Save result to : "))
+	saveTo = str(input(" [*] Save result to : "))
 	if saveTo.split(".")[-1] != "json": saveTo+=".json"
-	query = str(input(" Query : "))
-	pagination = int(input(" Pagination (1, 2, 3, ...): "))
-	data = scrap(query, pagination)
-	with open(saveTo, 'w', encoding='utf-8') as f:
-		json.dump(data, f, ensure_ascii=False, indent=4)
+	query = str(input(" [*] Keyword : "))
+	list_news(query, saveTo)
 	print(" Data was successfulyt saved to : "+saveTo)
 
 if __name__ == '__main__':
